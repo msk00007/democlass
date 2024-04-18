4e259f98c60e8c458e35f0f40b02017e412354c1
msk00007 <109849322+msk00007@users.noreply.github.com>
2024-04-18 12:16:33 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,59 @@
+// let age = 25;
+// console.log(age);
+
+// let price = 65.5;
+// console.log(price);
+
+// let name = "John";
+
+// console.log(name);
+
+// let isTrue = true;
+// console.log(isTrue);
+
+// let isFalse = false;
+// console.log(isFalse);
+
+// let arr = [1, 2, 3, 4, 5];
+// console.log(arr);
+
+// let x = 10;
+// let y = 5;
+
+// console.log(x == 5 && y == 5);
+// console.log(x == 5 || y == 5);
+// console.log(!(x == 5 && y == 5));
+// console.log(!(x == 5 || y == 5));
+
+// let arr = ["cse", "AI", "ML", "Ds"];
+// let length = arr.length;
+// for (let i = 0; i < length; i++) {
+//   console.log(arr[i]);
+// }
+
+// let i = 0;
+// while (i < length) {
+//   // execution code here
+// }
+
+// for (let i = 1; i <= 4; i++) {
+//   let pattern = "";
+//   for (let j = 1; j <= 4; j++) {
+//     if (i == 1 || i == 4 || j == 1 || j == 4) {
+//       pattern += "*";
+//     } else {
+//       pattern += " ";
+//     }
+//   }
+//   console.log(pattern);
+// }
+
+// https://github.com/msk00007/democlass
+
+function greet(name = "MRITS") {
+  return "Hello " + name;
+}
+let message = greet();
+let message2 = greet("sai");
+console.log(message);
+console.log(message2);
